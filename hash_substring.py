3bf594d99ccd62e27f1fa6805fc75ff09dcac22b
KristiansPriede221RDB394 <Kristians.Priede@edu.rtu.lv>
2023-04-10 20:16:43 +0300

    jjj

--- hash_substring.py
+++ hash_substring.py
@@ -1,17 +1,29 @@
 # python3
-
 path="/home/runner/work/string-pattern-KristiansPriede221RDB394/string-pattern-KristiansPriede221RDB394/tests/06"
 check=input()
-if check.startswith('I'):
-    pattern=input()
-    text=input()
-elif check.startswith('F'):
-    ptf=path
-    with open(ptf,'r')as file:
+if check.startswith('F'):
+    with open(path,'r')as file:
         lines=file.read()
         line=lines.split("\n",2)
         pattern=line[0] 
         text=line[1]
+elif check.startswith('I'):
+        pattern=input().rstrip()
+        text=input().rstrip()
+# this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
+    
+    # after input type choice
+    # read two lines 
+    # first line is pattern 
+    # second line is text in which to look for pattern 
+    
+    # return both lines in one return
+    
+    # this is the sample return, notice the rstrip function
+   ## return (input().rstrip(), input().rstrip())
+
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -51,4 +63,4 @@ def get_occurrences(pattern, text):
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(pattern, text))
+    print_occurrences(get_occurrences(pattern,text))
