a8a4419158136b46b977026625b1cdf59b37b248
KristiansPriede221RDB394 <Kristians.Priede@edu.rtu.lv>
2023-04-10 20:20:17 +0300

    cl

--- hash_substring.py
+++ hash_substring.py
@@ -10,20 +10,6 @@ if check.startswith('F'):
 elif check.startswith('I'):
         pattern=input().rstrip()
         text=input().rstrip()
-# this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-   ## return (input().rstrip(), input().rstrip())
-
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
